91ddc522b5f8b0e5c5a38a98ae7be4aa2d72b420
Olanrewaju <olanrewajuqudus5@gmail.com>
2024-06-29 22:01:20 +0100

    sequelize

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -1,31 +1,31 @@
 import { Sequelize } from "sequelize";
 
-// const sequelize = new Sequelize(
-//   process.env.POSTGRES_DATABASE_PROD!,
-//   process.env.POSTGRES_USER_NAME_PROD!,
-//   process.env.POSTGRES_PASSWORD_PROD!,
-//   {
-//     dialect: "postgres",
-//     port: +process.env.POSTGRES_PORT_PROD!,
-//     host: process.env.POSTGRES_HOST_PROD!,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
-
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE!,
-  process.env.POSTGRES_USER_NAME!,
-  process.env.POSTGRES_PASSWORD!,
+  process.env.POSTGRES_DATABASE_PROD!,
+  process.env.POSTGRES_USER_NAME_PROD!,
+  process.env.POSTGRES_PASSWORD_PROD!,
   {
     dialect: "postgres",
-    port: +process.env.POSTGRES_PORT!,
-    host: process.env.POSTGRES_HOST!,
+    port: +process.env.POSTGRES_PORT_PROD!,
+    host: process.env.POSTGRES_HOST_PROD!,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
+
+// const sequelize = new Sequelize(
+//   process.env.POSTGRES_DATABASE!,
+//   process.env.POSTGRES_USER_NAME!,
+//   process.env.POSTGRES_PASSWORD!,
+//   {
+//     dialect: "postgres",
+//     port: +process.env.POSTGRES_PORT!,
+//     host: process.env.POSTGRES_HOST!,
+//   }
+// );
 
 export default sequelize;
